817e742bbda4e4a2964d9b6b77c18648125ecc21
Kosuke Mizuno (WSL, Lab PC) <mizuno.k.ae@m.titech.ac.jp>
2020-02-23 01:06:20 +0900

    binary upload

--- src/esapy/upload.py
+++ src/esapy/upload.py
@@ -83,11 +83,46 @@ def upload_md(filename):
     pass
 
 
-def upload_image(filename):
-    pass
+def upload_binary(filename, token, team, *, logger=None):
+    logger = logger or default_logger
+
+    path_bin = Path(filename)
+    logger.info('uploading %s' % str(path_bin))
+    logger.info('  filesize: %d' % path_bin.stat().st_size)
+
+    # get metadata
+    url = 'https://api.esa.io/v1/teams/%s/attachments/policies' % team
+    header = dict(Authorization='Bearer %s' % token)
+    params = dict(type=mimetypes.guess_type(str(path_bin))[0],
+                  name=path_bin.name,
+                  size=path_bin.stat().st_size)
+    res = requests.post(url, headers=header, params=params)
+
+    if res.status_code != 200:
+        logger.warning('Obtaining metadata failed, %s' % str(path_bin))
+        raise RuntimeError('Obtaining metadata failed.')
+
+    metadata = res.json()
+    image_url = metadata['attachment']['url']
+
+    logger.info('  metadata is obtained.')
+    logger.debug(metadata)
+
+    # upload file
+    url = metadata['attachment']['endpoint']
+    with path_bin.open('rb') as imgfile:
+        params = metadata['form']
+        params['file'] = imgfile
+        res = requests.post(url, files=params)
+
+    if not (200 <= res.status_code < 300):
+        logger.warning('Upload failed, %s' % str(path_bin))
+        raise RuntimeError('Upload failed.')
+    logger.info('  the file is successfully uploaded, url: %s' % image_url)
+
+    return image_url
 
 
 if __name__ == '__main__':
     main()
 
-    upload_ipynb('/mnt/c/Users/MIZUNO/Documents/python/hatano_lab(GoogleDrive)/documents/instruments/S3-805 optics/gaussian beam.ipynb')
