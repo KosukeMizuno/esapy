953ce19089b717ef4ed350059ca173dbfe7fcc56
Kosuke Mizuno (WSL, Lab PC) <mizuno.k.ae@m.titech.ac.jp>
2020-05-10 21:56:31 +0900

    try-except for non-ipython context

--- src/esapy/__init__.py
+++ src/esapy/__init__.py
@@ -1,17 +1,20 @@
 __version__ = '1.2.0'
 
-from IPython.core.magic import register_line_cell_magic
 
+try:
+    from IPython.core.magic import register_line_cell_magic
 
-@register_line_cell_magic
-def esapy_fold(line, cell=None):
-    '''This magic is just a marker.
-    '''
-    if cell is None:
-        return line
-    else:
-        return line, cell
+    @register_line_cell_magic
+    def esapy_fold(line, cell=None):
+        '''This magic is just a marker.
+        '''
+        if cell is None:
+            return line
+        else:
+            return line, cell
 
+    def load_ipython_extension(ipython):
+        ipython.register_magics(esapy_fold)
 
-def load_ipython_extension(ipython):
-    ipython.register_magics(esapy_fold)
+except NameError:  # This register decorator can be called in ipython context.
+    pass
