dfc3d8a02d47fd0decb8865c7750d2e864acdc80
Kosuke Mizuno <kosuke.mizuno.nv@hco.ntt.co.jp>
2021-07-15 20:16:01 +0900

    rewrite api

--- src/esapy/api_growi.py
+++ src/esapy/api_growi.py
@@ -5,6 +5,8 @@ from pathlib import Path
 import mimetypes
 import requests
 import json
+import uuid
+import hashlib
 
 # logger
 from logging import getLogger
@@ -33,7 +35,7 @@ def get_team_stats(token=None, url=None, proxy=None):
                'checkServices': ['mongo', 'search'],
                'strictly': True}
 
-    res = requests.get(url + '/healthcheck', data=payload)
+    res = requests.get(url + '/healthcheck', data=json.dumps(payload))
     logger.debug(res)
     logger.debug(res.headers)
     if res.status_code == 200:
@@ -48,137 +50,109 @@ def get_team_stats(token=None, url=None, proxy=None):
     return res.text
 
 
-def upload_binary(filename, token=None, team=None, proxy=None):
+def upload_binary(filename, token=None, url=None, proxy=None):
     path_bin = Path(filename)
     logger.info('Uploading binary data, path=%s' % str(path_bin))
     logger.info('  filesize: %d' % path_bin.stat().st_size)
 
     _set_proxy(proxy)
-
-    # get metadata
-    logger.info('Obtaining metadata for upload...')
-    url = 'https://api.esa.io/v1/teams/%s/attachments/policies' % team
-    header = dict(Authorization='Bearer %s' % token)
-    mtype = mimetypes.guess_type(str(path_bin))[0]
-    mtype = mtype if mtype is not None else 'application/octet-stream'
-    params = dict(type=mtype,
-                  name=path_bin.name,
-                  size=path_bin.stat().st_size)
-    res = requests.post(url, headers=header, params=params)
-
-    if res.status_code != 200:
-        logger.warning('Obtaining metadata failed, %s' % str(path_bin))
-        raise RuntimeError('Obtaining metadata failed.')
-
-    metadata = res.json()
-    image_url = metadata['attachment']['url']
-
-    logger.info('  finished.')
-    logger.debug(metadata)
+    payload = {'access_token': token}
 
     # upload file
     logger.info('Posting binary...')
-    url = metadata['attachment']['endpoint']
     with path_bin.open('rb') as imgfile:
-        params = metadata['form']
-        params['file'] = imgfile
-        res = requests.post(url, files=params)
+        payload['file'] = imgfile
+        res = requests.post(url + '/attachments.add', data=payload)
 
-    if not (200 <= res.status_code < 300):
+    if res.status_code != 200:
         logger.warning('Upload failed, %s' % str(path_bin))
         raise RuntimeError('Upload failed.')
+
+    image_url = res.json()['attachment']['filePathProxied']
     logger.info('The file has been uploaded successfully, url: %s' % image_url)
 
     return image_url, res
 
 
-def get_post(post_number, token=None, team=None, proxy=None):
-    logger.info('Getting post/{:d}'.format(post_number))
+def get_post(page_id, token=None, url=None, proxy=None):
+    logger.info('Getting post/{:d}'.format(page_id))
 
+    # post
     _set_proxy(proxy)
+    payload = {'access_token': token,
+               'page_id': page_id,
+               }
 
-    # get metadata
-    url = 'https://api.esa.io/v1/teams/{:s}/posts/{:d}'.format(team, post_number)
-    header = dict(Authorization='Bearer {:s}'.format(token))
+    res = requests.post(url + '/pages.get',
+                        data=json.dumps(payload))
+    logger.debug(res)
 
-    res = requests.get(url, headers=header)
     if res.status_code != 200:
         logger.warning('Getting post failed.')
         raise RuntimeError('Getting post failed.')
-    logger.info(res)
 
     d = res.json()
     logger.debug(d)
 
-    return d
+    return d['page']
 
 
-def create_post(body_md, token=None, team=None, name=None, tags=None, category=None, wip=True, message=None, proxy=None):
+def create_post(body_md, token=None, url=None, name=None, proxy=None):
     logger.info('Creating new post')
 
     # post
     _set_proxy(proxy)
-    url = 'https://api.esa.io/v1/teams/%s/posts' % team
-    header = {'Authorization': 'Bearer %s' % token,
-              'Content-Type': 'application/json'}
 
-    params = dict(post=dict(name=name or "",
-                            message=message or "Create post via esapy",
-                            body_md=body_md,
-                            wip=wip))
-    if isinstance(tags, list) and all([isinstance(t, str) for t in tags]):
-        params['post']['tags'] = tags
-    if category is not None:
-        params['post']['category'] = category
+    if name is None:
+        raise RuntimeError('`name` is required.')
+    payload = {'access_token': token,
+               'body': body_md,
+               'path': '/user/bot/' + name,
+               'grant': 1  # なにこれ？ とりあえず1にしておいたけどドキュメントがない
+               }
 
-    res = requests.post(url, headers=header, data=json.dumps(params))
+    res = requests.post(url + '/create',
+                        data=json.dumps(payload))
     logger.debug(res)
 
-    if res.status_code != 201:
+    if res.status_code == 409:
+        raise RuntimeError('Page path is already existed. Retry with different name.')
+    elif res.status_code != 201:
         raise RuntimeError('Create post failed.')
     logger.info('New post was successfully created.')
 
     d = res.json()
-    logger.debug(dict(d, **{'body_md': '<not shown>', 'body_html': '<not shown>'}))
-    logger.info('URL of the created post: %s' % d['url'])
+    logger.debug(d['page'])
+    pageurl = url + '/' + d['page']['path']
+    logger.info('URL of the created post: %s' % pageurl)
 
-    return d['url'], res
+    return pageurl, res
 
 
-def patch_post(post_number, body_md, token=None, team=None, name=None, tags=None, category=None, wip=True, message=None, proxy=None):
-    logger.info('Updating post/{:d}'.format(post_number))
+def patch_post(page_id, body_md, token=None, url=None, proxy=None):
+    logger.info('Updating post')
 
     # post
     _set_proxy(proxy)
-    url = 'https://api.esa.io/v1/teams/{:s}/posts/{:d}'.format(team, post_number)
-    header = {'Authorization': 'Bearer {:s}'.format(token),
-              'Content-Type': 'application/json'}
+    rev = hashlib.sha256(uuid.uuid4()).hexdigest()
+    payload = {'access_token': token,
+               'body': body_md,
+               'page_id': page_id,
+               'revision_id': rev,
+               'grant': 1  # なにこれ？ とりあえず1にしておいたけどドキュメントがない
+               }
 
-    params = dict(post=dict(name=name or "",
-                            message=message or "Update post via esapy",
-                            body_md=body_md,
-                            wip=wip))
-    if len(params['post']['name']) > 0 and params['post']['name'][0] != '/':
-        params['post']['name'] = '/' + params['post']['name']
-    if isinstance(tags, list) and all([isinstance(t, str) for t in tags]):
-        params['post']['tags'] = tags
-    if category is not None:
-        params['post']['category'] = category
-
-    res = requests.patch(url, headers=header, data=json.dumps(params))
+    res = requests.post(url + '/pages.update',
+                        data=json.dumps(payload))
     logger.debug(res)
 
     if res.status_code != 200:
-        print(res)
-        print(res.json())
         raise RuntimeError('Create post failed.')
-    logger.info('Patching post was successfully created.')
+    logger.info('New post was successfully created.')
 
     d = res.json()
-    logger.debug(dict(d, **{'body_md': '<not shown>', 'body_html': '<not shown>'}))
-    logger.info('URL of the created post: %s' % d['url'])
+    logger.debug(d['page'])
+    pageurl = url + '/' + d['page']['path']
+    logger.info('URL of the created post: %s' % pageurl)
 
-    if d['overlapped']:
-        logger.warning('!!! 3 way merge and conflicting has been occured !!!')
-
-    return d['url'], res
+    return pageurl, res
